76b0978b0c0a3cc81caac5eced002b428c643e62
Satoshi Amemiya <s.amemiya@green-bell.jp>
2013-12-05 17:45:09 +0900

    capistranoの設定を作成

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+require "bundler/capistrano"
+
+set :application, "clio"
+set :repository,  "git://github.com/greenbell/clio.git"
+set :scm, :git
+set :use_sudo, false
+
+role :web, "clio@192.168.11.197"
+role :app, "clio@192.168.11.197"
+role :db, "clio@192.168.11.197", :primary => true
+set :gateway, "clio@180.211.76.102"
+set :ssh_options, {
+  :auth_methods => %w(publickey)
+}
+
+set :deploy_to, "/var/www/clio/clio/"
+namespace :deploy do
+  task :start do
+    run "service unicorn_clio_clio start" 
+  end
+  task :stop do
+    run "service unicorn_clio_clio stop"
+  end
+  task :restart do
+    run "service unicorn_clio_clio restart"
+  end
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/mongoid.yml #{latest_release}/config/mongoid.yml"
+  end
+end
