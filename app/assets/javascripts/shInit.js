77d156ef7201f5f5d155f3184631c97896302503
Satoshi Amemiya <s.amemiya@green-bell.jp>
2013-12-06 14:52:25 +0900

    mysql.slow、sql文の表示方法を変更

--- app/assets/javascripts/shInit.js
+++ app/assets/javascripts/shInit.js
@@ -1,6 +1,4 @@
-SyntaxHighlighter.config.strings.expandSource = "クリックで展開";
-SyntaxHighlighter.defaults['gutter'] = false;
-SyntaxHighlighter.defaults['collapse'] = true;
 $(function () {
+  SyntaxHighlighter.defaults['gutter'] = false;
   SyntaxHighlighter.all()
 });
