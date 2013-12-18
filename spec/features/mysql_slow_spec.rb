780ebafdadda166f0bdff77a5e7598e9cca157f0
Satoshi Amemiya <s.amemiya@green-bell.jp>
2013-12-18 16:05:33 +0900

    mysql.slowのテストを修正・追加

--- spec/features/mysql_slow_spec.rb
+++ spec/features/mysql_slow_spec.rb
@@ -8,7 +8,7 @@ describe 'mysql_slow' do
   subject { page }
 
   describe '#index' do
-    let(:path) { mysql_slow_index_path }
+    let(:path) { log_path(:mysql_slow) }
     it "shows log exactly" do
       log = create(:mysql_slow, :today)
       visit path
@@ -18,46 +18,128 @@ describe 'mysql_slow' do
     it "paginates exactly" do
       logs = 31.times.map { create(:mysql_slow, :today) }
       logs.sort_by! {|log| DateTime.now - log.time}
-      visit mysql_slow_index_path(:page => 2)
+      visit log_path(:mysql_slow, :page => 2)
       find("#mysql_slow_table > tbody").should have_text(format_log(logs[-1]))
       all(".pagination").first.should have_text("« First ‹ Prev 1 2")
     end
 
-    context "when sorted by asc of lock_time" do
-      let(:path) { mysql_slow_index_path(:sort => "a-lock_time") }
-      it "shows logs with sorted by asc of lock_time" do
-        logs = 5.times.map { create(:mysql_slow, :today) }
-        logs.sort_by! {|log| log.lock_time }
-        visit path
-        logs.each_with_index do |log, i|
-          find("#mysql_slow_table > tbody > tr:nth-child(#{2*i+1})").should have_text(format_log(log))
+    context "when sorted by" do
+      let!(:logs) { 5.times.map { create(:mysql_slow, :today) }}
+      shared_examples_for "sorting" do
+        it "shows sorted logs" do
+          logs.sort_by! &order
+          visit path
+          logs.each_with_index do |log, i|
+            find("#mysql_slow_table > tbody > tr:nth-child(#{2*i+1})").should have_text(format_log(log))
+          end
         end
+      end
+
+      context "asc of time" do
+        let(:path) { log_path(:mysql_slow, :sort => "a-time") }
+        let(:order) { lambda {|log| log.time }}
+        include_examples "sorting"
+      end
+
+      context "desc of time" do
+        let(:path) { log_path(:mysql_slow, :sort => "d-time") }
+        let(:order) { lambda {|log| -log.time.to_f }}
+        include_examples "sorting"
+      end
+
+      context "asc of query_time" do
+        let(:path) { log_path(:mysql_slow, :sort => "a-query_time") }
+        let(:order) { lambda {|log| log.query_time }}
+        include_examples "sorting"
+      end
+
+      context "desc of query_time" do
+        let(:path) { log_path(:mysql_slow, :sort => "d-query_time") }
+        let(:order) { lambda {|log| -log.query_time }}
+        include_examples "sorting"
+      end
+
+      context "asc of lock_time" do
+        let(:path) { log_path(:mysql_slow, :sort => "a-lock_time") }
+        let(:order) { lambda {|log| log.lock_time }}
+        include_examples "sorting"
+      end
+
+      context "desc of lock_time" do
+        let(:path) { log_path(:mysql_slow, :sort => "d-lock_time") }
+        let(:order) { lambda {|log| -log.lock_time }}
+        include_examples "sorting"
+      end
+
+      context "asc of rows_sent" do
+        let(:path) { log_path(:mysql_slow, :sort => "a-rows_sent") }
+        let(:order) { lambda {|log| log.rows_sent }}
+        include_examples "sorting"
+      end
+
+      context "desc of rows_sent" do
+        let(:path) { log_path(:mysql_slow, :sort => "d-rows_sent") }
+        let(:order) { lambda {|log| -log.rows_sent }}
+        include_examples "sorting"
+      end
+
+      context "asc of rows_examined" do
+        let(:path) { log_path(:mysql_slow, :sort => "a-rows_examined") }
+        let(:order) { lambda {|log| log.rows_examined }}
+        include_examples "sorting"
+      end
+
+      context "desc of rows_examined" do
+        let(:path) { log_path(:mysql_slow, :sort => "d-rows_examined") }
+        let(:order) { lambda {|log| -log.rows_examined }}
+        include_examples "sorting"
       end
     end
 
     context "when specified today" do
-      let(:path) { mysql_slow_index_path(:date => Date.today.strftime("%Y/%m/%d")) }
+      let(:path) { log_path(:mysql_slow, :date => Date.today.strftime("%Y/%m/%d")) }
       it "shows only logs are created today" do
         today = create(:mysql_slow, :today)
         dummy = create(:mysql_slow, :time => rand(DateTime.now.to_f - Date.today.to_time.to_f).ago - 1.day)
         visit path
-        within(:css, "#mysql_slow_table") do
+        within(:css, "#mysql_slow_table > tbody") do
           should have_text(format_log(today))
           should_not have_text(format_log(dummy))
         end
       end
     end
 
-    context "when filtered by host" do
-      let(:log) { create(:mysql_slow, :today) }
-      let(:path) { mysql_slow_index_path(:"filter[host]" => log.host) }
-      it "shows only logs whose host is specified" do
-        dummy = create(:mysql_slow, :today)
-        visit path
-        within(:css, "#mysql_slow_table") do
-          should have_text(format_log(log))
-          should_not have_text(format_log(dummy))
+    context "when filtered by value of" do
+      let!(:log) { create(:mysql_slow, :today) }
+      let!(:dummy) { create(:mysql_slow, :today) }
+      shared_examples_for "filtering" do
+        it "shows only logs that fulfill" do
+          visit path
+          within(:css, "#mysql_slow_table > tbody") do
+            should have_text(format_log(log))
+            should_not have_text(format_log(dummy))
+          end
         end
+      end
+
+      context "server_name" do
+        let(:path) { log_path(:mysql_slow, :"filter[server_name]" => log.server_name) }
+        include_examples "filtering"
+      end
+
+      context "user" do
+        let(:path) { log_path(:mysql_slow, :"filter[user]" => log.user) }
+        include_examples "filtering"
+      end
+
+      context "host" do
+        let(:path) { log_path(:mysql_slow, :"filter[host]" => log.host) }
+        include_examples "filtering"
+      end
+
+      context "host_ip" do
+        let(:path) { log_path(:mysql_slow, :"filter[host_ip]" => log.host_ip) }
+        include_examples "filtering"
       end
     end
   end
