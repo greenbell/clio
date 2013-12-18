95679e3549925d9b8f8baa7e7171d3e5818a33c8
Satoshi Amemiya <s.amemiya@green-bell.jp>
2013-12-18 16:11:13 +0900

    maillogのテストを修正・追加

--- spec/features/maillog_spec.rb
+++ spec/features/maillog_spec.rb
@@ -8,7 +8,7 @@ describe 'maillog' do
   subject { page }
 
   describe '#index' do
-    let(:path) { maillog_index_path }
+    let(:path) { log_path(:maillog) }
     it "shows log exactly" do
       log = create(:maillog, :today)
       visit path
@@ -18,46 +18,70 @@ describe 'maillog' do
     it "paginates exactly" do
       logs = 31.times.map { create(:maillog, :today) }
       logs.sort_by! {|log| DateTime.now - log.time}
-      visit maillog_index_path(:page => 2)
+      visit log_path(:maillog, :page => 2)
       find("#maillog_table > tbody").should have_text(format_log(logs[-1]))
       all(".pagination").first.should have_text("« First ‹ Prev 1 2")
     end
 
-    context "when sorted by asc of time" do
-      let(:path) { maillog_index_path(:sort => "a-time") }
-      it "shows logs with sorted by asc of time" do
-        logs = 5.times.map { create(:maillog, :today) }
-        logs.sort_by! {|log| log.time }
-        visit path
-        logs.each_with_index do |log, i|
-          find("#maillog_table > tbody > tr:nth-child(#{i+1})").should have_text(format_log(log))
+    context "when sorted by" do
+      let!(:logs) { 5.times.map { create(:maillog, :today) }}
+      shared_examples_for "sorting" do
+        it "shows sorted logs" do
+          logs.sort_by! &order
+          visit path
+          logs.each_with_index do |log, i|
+            find("#maillog_table > tbody > tr:nth-child(#{i+1})").should have_text(format_log(log))
+          end
         end
+      end
+
+      context "asc of time" do
+        let(:path) { log_path(:maillog, :sort => "a-time") }
+        let(:order) { lambda {|log| log.time }}
+        include_examples "sorting"
+      end
+
+      context "desc of time" do
+        let(:path) { log_path(:maillog, :sort => "d-time") }
+        let(:order) { lambda {|log| -log.time.to_f }}
+        include_examples "sorting"
       end
     end
 
     context "when specified today" do
-      let(:path) { maillog_index_path(:date => Date.today.strftime("%Y/%m/%d")) }
+      let(:path) { log_path(:maillog, :date => Date.today.strftime("%Y/%m/%d")) }
       it "shows only logs are created today" do
         today = create(:maillog, :today)
         dummy = create(:maillog, :time => rand(DateTime.now.to_f - Date.today.to_time.to_f).ago - 1.day)
         visit path
-        within(:css, "#maillog_table") do
+        within(:css, "#maillog_table > tbody") do
           should have_text(format_log(today))
           should_not have_text(format_log(dummy))
         end
       end
     end
 
-    context "when filtered by server_name" do
-      let(:log) { create(:maillog, :today) }
-      let(:path) { maillog_index_path(:"filter[server_name]" => log.server_name) }
-      it "shows only logs whose server_name is specified" do
-        dummy = create(:maillog, :today)
-        visit path
-        within(:css, "#maillog_table") do
-          should have_text(format_log(log))
-          should_not have_text(format_log(dummy))
+    context "when filtered by value of" do
+      let!(:log) { create(:maillog, :today) }
+      let!(:dummy) { create(:maillog, :today) }
+      shared_examples_for "filtering" do
+        it "shows only logs that fulfill" do
+          visit path
+          within(:css, "#maillog_table > tbody") do
+            should have_text(format_log(log))
+            should_not have_text(format_log(dummy))
+          end
         end
+      end
+
+      context "server_name" do
+        let(:path) { log_path(:maillog, :"filter[server_name]" => log.server_name) }
+        include_examples "filtering"
+      end
+
+      context "daemon" do
+        let(:path) { log_path(:maillog, :"filter[daemon]" => log.daemon) }
+        include_examples "filtering"
       end
     end
   end
