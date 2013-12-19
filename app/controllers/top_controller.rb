353afac0fc3e9d3a4d41c1c5af8368f400fb1260
Satoshi Amemiya <s.amemiya@green-bell.jp>
2013-12-19 12:55:38 +0900

    staging等のログを見ると、トップページで閲覧できるログも変わってしまっていた

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,8 @@
 
 class TopController < ApplicationController
   def index
-    @apache_access_logs = ApacheAccess.desc(:time).limit(5)
-    @mysql_slow_logs = MysqlSlow.desc(:time).limit(5)
-    @maillog_logs = Maillog.desc(:time).limit(5)
+    @apache_access_logs = ApacheAccess.set_session("default").desc(:time).limit(5)
+    @mysql_slow_logs = MysqlSlow.set_session("default").desc(:time).limit(5)
+    @maillog_logs = Maillog.set_session("default").desc(:time).limit(5)
   end
 end
