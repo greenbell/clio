8d229e7beeea5c02aef5b9fdd18b2ff4cfe0f967
Satoshi Amemiya <s.amemiya@green-bell.jp>
2013-12-27 18:05:40 +0900

    Graphクラスを作成

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.0.1'
+gem 'activeresource'
 gem 'mongoid', git: 'https://github.com/mongoid/mongoid.git'
 gem 'kaminari'
 gem 'uglifier', '>= 1.3.0'
---
+++ app/models/graph.rb
@@ -0,0 +1,29 @@
+require 'active_resource'
+require 'base64'
+
+class Graph < ActiveResource::Base
+  class Format
+    def extension() "" end
+    def mime_type() "image/png" end
+    def decode(image)
+      {:dataurl => "data:image/png;base64,#{Base64.encode64(image).gsub("\n", "")}"}
+    end
+  end
+
+  def self.element_path(id, prefix_options = {}, query_options = nil)
+    prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+    "#{prefix(prefix_options)}#{collection_name}/#{URI.escape id.to_s}#{query_string(query_options)}"
+  end
+
+  def self.select_collection(collection_name)
+    self.collection_name = collection_name
+    self
+  end
+
+  self.site = "http://0.0.0.0:5125/"
+  self.prefix = "/graph/log/"
+  self.format = Format.new
+  self.logger = Logger.new($stderr)
+end
+
+    
