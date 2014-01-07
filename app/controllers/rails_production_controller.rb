22936e072457f584ab6df18535b201e8d2d1f022
Satoshi Amemiya <s.amemiya@green-bell.jp>
2014-01-07 17:12:35 +0900

    railsアプリのグラフを表示

--- app/controllers/rails_production_controller.rb
+++ app/controllers/rails_production_controller.rb
@@ -2,6 +2,7 @@
 
 class RailsProductionController < ApplicationController
   def index
+    @graph = Graph.select_collection("rails.production").find(params[:filter][:app]) if params[:filter][:app].present? if params[:filter]
     @logs = RailsProduction.set_session(params[:session])
                            .filter_by_datetime(params[:datetime])
                            .filter_by_level(params[:level])
